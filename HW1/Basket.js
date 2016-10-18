39e3753123a0ded4167bb425fc13776d0bce9949
Valeriy Posvistak <kiev.junta.14@gmail.com>
2016-10-18 12:32:10 +0300

    ssRevert "Merge branch 'master' of https://github.com/abyrvalg/kisit_web_course_2016"
    This reverts commit 0b7282fcc1b0ed13f901ceb885c334c7cf616c71, reversing
    changes made to 5e739bc24e448e2ad7b4b355da4ba1f0859e325a.

--- HW1/Basket.js
+++ HW1/Basket.js
@@ -1,126 +1,35 @@
-//Installed node modules: jquery underscore request express jade shelljs passport http sys lodash async mocha chai sinon sinon-chai moment connect validator restify ejs ws co when helmet wrench brain mustache should backbone forever debug
 var prodcuts = [{
-    name: "mouse",
-    price: 5,
-    inventory: 1
+	name : "test",
+	price : 12.9,
+	inventory : 20
 }, {
-        name: "laptop",
-        price: 275,
-        inventory: 80
-    }, {
-        name: "usbAdapter",
-        price: 3,
-        inventory: 50
-    }, {
-        name: "keyboard",
-        price: 6,
-        inventory: 0
-    }];
+	name : "test2",
+	price : 30,
+	inventory : 80
+}];
 
 function ProductLineItem(product) {
-    this.name = product.name;
-    this.price = product.price;
-    this.inventory = 1;
+	//implement
 }
 
 ProductLineItem.prototype = {
-    ShowInfo: function () {
-        console.log("Name: " + this.name + " ..price: " + this.price + " ..inventory: " + this.inventory);
-    }
+	//implement
 };
 
-var basket = (function () {
-
-    var productLineItems = [];
-
-    return {
-        addProduct: function (productId) {
-            if (!(productId in prodcuts) || prodcuts[productId].inventory == 0)
-                return false;
-
-
-            var itemIndex = this.getIdOf(prodcuts[productId].name);
-
-
-            if (itemIndex == -1)
-                productLineItems.push(new ProductLineItem(prodcuts[productId]))
-            else
-                productLineItems[itemIndex].inventory++;
-
-            prodcuts[productId].inventory--;
-            return true;
-        },
-
-        removeProduct: function (productId) {
-
-            if (!(productId in prodcuts))
-                return false;
-
-            var itemIndex = this.getIdOf(prodcuts[productId].name);
-
-            if (itemIndex == -1)
-                return false;
-
-            if (productLineItems[itemIndex].inventory > 0)
-                productLineItems[itemIndex].inventory--;
-
-            return true;
-
-        },
-
-        updateProductQuantity: function (productId, quantity) {
-
-            if (!(productId in prodcuts))
-                return false;
-
-            var itemIndex = this.getIdOf(prodcuts[productId].name);
-
-            if (itemIndex == -1)
-                return false;
-
-            productLineItems[itemIndex].inventory = quantity;
-
-            return true;
-        },
-
-        getTotalPrice: function () {
-
-            var totalPrice = 0;
-
-            for (var i = 0; i < productLineItems.length; i++) {
-
-                totalPrice += productLineItems[i].price * productLineItems[i].inventory;
-            }
-
-            return totalPrice;
-        },
-
-        getIdOf: function (productName) {
-
-            for (var i = 0; i < productLineItems.length; i++) {
-                if (productLineItems[i].name == productName)
-                    return i;
-            }
-
-            return -1;
-        },
-
-        ShowAllInfo: function () {
-            for (var i = 0; i < productLineItems.length; i++) {
-                productLineItems[i].ShowInfo();
-            }
-        }/*,
-        contains : function(productName){
-            return getIdOf(productName)!=1;
-        } */
-    }
+var basket = (function(){
+	//implement
+	return {
+		addProduct : function(productID){
+			//implement
+		},
+		removeProduct : function(productID){
+			//implement
+		},
+		updateProductQuantity : function(productID, quantity) {
+			//implement
+		},
+		getTotalPrice : function(){
+			//implement
+		}
+	}
 })();
-
-console.log("Adding 2.. " + basket.addProduct(2));
-console.log("Removing 2.. " + basket.removeProduct(2));
-console.log("Updating 2 quantity to 1.. " + basket.updateProductQuantity(2, 1));
-console.log("Trying to add unknown id.. " + basket.addProduct(228));
-console.log("Adding 0.. " + basket.addProduct(0));
-console.log("Trying to add 0 (the item is over).. " + basket.addProduct(0));
-console.log("\nTotal price.. " + basket.getTotalPrice());
-basket.ShowAllInfo();
